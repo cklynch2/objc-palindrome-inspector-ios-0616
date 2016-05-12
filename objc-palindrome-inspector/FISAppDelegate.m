22520daa6e4abec30dcc4371d3d970db9d225cda
cklynch2 <cklynch2@gmail.com>
2016-05-12 09:50:38 -0400

    Palindrome detector solved using test suite

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,32 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+-(BOOL)stringIsPalindrome:(NSString *)string
+{
+    NSArray *punctuations = @[@".", @",", @"!", @"?", @";", @":", @" "];
+    NSString *noPunctuation = [string copy];
+    for(NSUInteger i = 0; i < [punctuations count]; i++){
+//        NSString *punctuation = punctuations[i];
+        noPunctuation = [noPunctuation stringByReplacingOccurrencesOfString: punctuations[i] withString:@""];
+    }
+        
+    
+    NSString *lowercase = [noPunctuation lowercaseString];
+    NSString *reverse = [self stringByReversingString:lowercase];
+    BOOL stringIsPalindrome = [lowercase isEqualToString:reverse];
+    return stringIsPalindrome;
+}
+
+-(NSString *)stringByReversingString:(NSString *)string
+{
+    NSString *result = @"";
+    for (NSUInteger i = [string length]; i > 0; i--){
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c",c];
+    }
+    return result;
+}
 
 @end
